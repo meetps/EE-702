Remove p,q sign bug

shape-from-shading/shapeFromShading.py
@@ -12,7 +12,7 @@ source = [0,0,1] 			# Coordinate of Light Source
 Lambda = 0.001				# Regularization Parameter
 noiseSNR = 5; 				# Noise to signal ratio
 radiusToImageRatio = 0.5	# Radius to Image dimensions ratio
-sphereImageSize = 100      # Radius of the spehere to be rendered
+sphereImageSize = 50     # Radius of the spehere to be rendered
 
 #######################################################
 # Rendering the 3D Surface
@@ -42,6 +42,7 @@ for i in range(1,sphereImageSize-1):
 		p[i][j] = ( depthMap[i][j+1] - depthMap[i][j-1] )/ 2 * regionOfInterest[i][j]
 		q[i][j] = ( depthMap[i-1][j] - depthMap[i+1][j] )/ 2 * regionOfInterest[i][j]
 
+p,q = p * regionOfInterest, q * regionOfInterest
 # print(p)
 #######################################################
 # Calculating the image radiance from gradient fields
@@ -49,7 +50,7 @@ for i in range(1,sphereImageSize-1):
 radiance = np.zeros((sphereImageSize,sphereImageSize))
 for i in range(0,sphereImageSize):
 	for j in range(0,sphereImageSize):
-		if(regionOfInterest[i][j] > 0):
+		if(regionOfInterest[i][j]):
 			radiance[i][j] = (p[i,j]*source[0] + q[i,j]*source[1] + 1)/(((source[0]**2+source[1]**2 + 1)**0.5)*((p[i,j]**2 + q[i,j]**2 + 1)**0.5))
 			if (radiance[i][j] < 0 ):
 				radiance[i][j] = 0
@@ -61,8 +62,8 @@ for i in range(0,sphereImageSize):
 boundaryMap              = np.zeros((sphereImageSize,sphereImageSize))
 regionOfInterestRadiance = radiance > 0
 kernel                   = np.ones((3,3),np.uint8)
-boundaryMap              = regionOfInterestRadiance - cv2.erode(regionOfInterestRadiance.astype(np.uint8),kernel,10)
-boundaryMap              = cv2.erode(cv2.dilate(boundaryMap.astype(np.uint8),kernel,10),kernel,10) 
+boundaryMap              = regionOfInterestRadiance - cv2.erode(regionOfInterestRadiance.astype(np.uint8),kernel,2)
+boundaryMap              = cv2.erode(cv2.dilate(boundaryMap.astype(np.uint8),kernel,2),kernel,2) 
 # print(sum(sum(boundaryMap)))
 
 #######################################################
@@ -90,9 +91,10 @@ for iteration in range(0,limit):
 	for i in range(1,pBoundary.shape[0] -1):
 		for j in range(1,pBoundary.shape[1] -1):
 			if regionOfInterestRadiance[i][j] == 1 :
+				RadianceX,RadianceY = 0,0
 				radianc = (p_estimated[i][j] * source[0] + q_estimated[i][j] * source[1] + 1) / ( ((source[0]**2+source[1]**2 + 1)**0.5) *((p_estimated[i][j]**2 + q_estimated[i][j]**2 + 1)**0.5))
 				RadianceX = (p_estimated[i][j]**2 *source[0] + source[0] - q_estimated[i][j]*q_estimated[i][j]*source[1] - p_estimated[i][j])/(((source[0]**2 + source[1]**2 + 1)**0.5)*(p_estimated[i][j]**2 + (q_estimated[i][j]**2 + 1)**0.5)**3)
-				RadianceY = (q_estimated[i][j]**2 *source[0] + source[0] - p_estimated[i][j]*p_estimated[i][j]*source[1] - q_estimated[i][j])/(((source[0]**2 + source[1]**2 + 1)**0.5)*(p_estimated[i][j]**2 + (q_estimated[i][j]**2 + 1)**0.5)**3)
+				RadianceY = (q_estimated[i][j]**2 *source[1] + source[1] - p_estimated[i][j]*p_estimated[i][j]*source[0] - q_estimated[i][j])/(((source[0]**2 + source[1]**2 + 1)**0.5)*(p_estimated[i][j]**2 + (q_estimated[i][j]**2 + 1)**0.5)**3)
 				p_next[i][j] = 0.25*(p_estimated[i-1][j] + p_estimated[i+1][j] + p_estimated[i][j-1] + p_estimated[i][j+1]) + 1/Lambda *(radiance[i][j] - radianc)*RadianceX;
 				q_next[i][j] = 0.25*(q_estimated[i-1][j] + q_estimated[i+1][j] + q_estimated[i][j-1] + q_estimated[i][j+1]) + 1/Lambda *(radiance[i][j] - radianc)*RadianceY;
 	p_estimated = (p_next*regionOfInterestRadiance*(1-boundaryMap.astype(bool))) + pBoundary*boundaryMap*regionOfInterestRadiance
@@ -120,20 +122,13 @@ Z_estimated = Z * regionOfInterestRadiance
 Z_estimated = -Z_estimated
 
 # print(q_y[sphereImageSize/2])
+print(Z_p[sphereImageSize/2])
 # print(p_x[sphereImageSize/2])
 #######################################################
 # Visualization of the Depth
 #######################################################
 fig = plt.figure()
 ax = fig.gca(projection='3d')
-surf = ax.plot_surface(cols, rows, Z_estimated, rstride=1, cstride=1, cmap=cm.coolwarm,linewidth=0, antialiased=False)
+surf = ax.plot_surface(rows, cols, Z_estimated, rstride=1, cstride=1, cmap=cm.coolwarm,linewidth=0, antialiased=False)
 fig.colorbar(surf, shrink=0.5, aspect=5)
 plt.show()
-
-
-
-
-
-
-
-