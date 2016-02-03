3d95789b7a5aefc7ea92753fee463132ba0b3d84
meetshah1995 <meetshah1995@iitb.ac.in>
2016-02-03 20:10:35 +0530

    Add improvements and remove reference array bug

--- shape-from-shading/shapeFromShading.py
+++ shape-from-shading/shapeFromShading.py
@@ -9,17 +9,19 @@ from matplotlib import cm
 # Parameter Definition
 #######################################################
 source = [0,0,1] 			# Coordinate of Light Source
-Lambda = 0.001				# Regularization Parameter
+Lambda = 100				# Regularization Parameter
 noiseSNR = 5; 				# Noise to signal ratio
-radiusToImageRatio = 0.5	# Radius to Image dimensions ratio
-sphereImageSize = 50     # Radius of the spehere to be rendered
+radiusToImageRatio = 0.25	# Radius to Image dimensions ratio
+sphereImageSize = 75   # Radius of the spehere to be rendered
 
 #######################################################
 # Rendering the 3D Surface
 #######################################################
 depthMap         = np.zeros((sphereImageSize,sphereImageSize))                     # Array to store depth (z) values
+clippingMap         = np.zeros((sphereImageSize,sphereImageSize))                     # Array to store depth (z) values
 regionOfInterest = np.zeros((sphereImageSize,sphereImageSize))                     # Boolean flag to mark the sphere ROI
 radius           = radiusToImageRatio * sphereImageSize                            # Radius of the sphere
+clippingRadius   = radius * 0.95
 [cols,rows] 	 = np.meshgrid(range(0,sphereImageSize),range(0,sphereImageSize))  # Meshgrid for base of computation
 
 #Calculating the depth using z^2 = r^2 - x^2 - y^2  for each point in the depth map
@@ -30,6 +32,17 @@ for i in range(0,sphereImageSize):
 			regionOfInterest[i][j] = 1;
 
 depthMap = np.sqrt(depthMap * regionOfInterest)
+
+
+for i in range(0,sphereImageSize):
+	for j in range(0,sphereImageSize):
+		clippingMap[i][j] = clippingRadius**2 - math.pow(cols[i][j] - sphereImageSize/2 , 2) - math.pow(rows[i][j] - sphereImageSize/2 , 2);
+		if(clippingMap[i][j] > 0):
+			regionOfInterest[i][j] = 1;
+
+
+depthMap = depthMap * regionOfInterest
+
 # print(depthMap)
 # print(regionOfInterest)
 
@@ -39,15 +52,15 @@ depthMap = np.sqrt(depthMap * regionOfInterest)
 p,q = np.zeros((sphereImageSize,sphereImageSize)),np.zeros((sphereImageSize,sphereImageSize))
 for i in range(1,sphereImageSize-1):
 	for j in range(1,sphereImageSize-1):
-		p[i][j] = ( depthMap[i][j+1] - depthMap[i][j-1] )/ 2 * regionOfInterest[i][j]
-		q[i][j] = ( depthMap[i-1][j] - depthMap[i+1][j] )/ 2 * regionOfInterest[i][j]
+		p[i][j] = depthMap[i][j] - depthMap[i][j-1]
+		q[i][j] = depthMap[i][j] - depthMap[i-1][j]  
 
 p,q = p * regionOfInterest, q * regionOfInterest
 # print(p)
 #######################################################
 # Calculating the image radiance from gradient fields
 #######################################################
-radiance = np.zeros((sphereImageSize,sphereImageSize))
+radiance = np.zeros((sphereImageSize,sphereImageSize))	
 for i in range(0,sphereImageSize):
 	for j in range(0,sphereImageSize):
 		if(regionOfInterest[i][j]):
@@ -62,29 +75,35 @@ for i in range(0,sphereImageSize):
 boundaryMap              = np.zeros((sphereImageSize,sphereImageSize))
 regionOfInterestRadiance = radiance > 0
 kernel                   = np.ones((3,3),np.uint8)
-boundaryMap              = regionOfInterestRadiance - cv2.erode(regionOfInterestRadiance.astype(np.uint8),kernel,2)
-boundaryMap              = cv2.erode(cv2.dilate(boundaryMap.astype(np.uint8),kernel,2),kernel,2) 
+boundaryMap              = regionOfInterestRadiance - cv2.erode(regionOfInterestRadiance.astype(np.uint8),kernel,5)
+boundaryMap              = cv2.erode(cv2.dilate(boundaryMap.astype(np.uint8),kernel,3),kernel,3) 
+intersectionROI          = regionOfInterest * regionOfInterestRadiance
+p,q = p * intersectionROI , q * intersectionROI
+
 # print(sum(sum(boundaryMap)))
+
 
 #######################################################
 # Occluding Boundary gradients
 #######################################################
-gradX, gradY       = radiance, radiance
+# gradX, gradY       = radiance, radiance
+gradX, gradY       = np.array(depthMap,copy=True),np.array(depthMap,copy=True)
 gradX[:][1:-1]     = (  gradX[:][2:]   - gradX[:][:-2]) * 0.5 
 gradY[1:-1][:]     = (  gradY[:-2][:]  - gradY[2:][:] ) * 0.5
-gradX = gradX * boundaryMap.astype(bool)
-gradY = gradY * boundaryMap.astype(bool)
-pBoundary = gradX * boundaryMap.astype(bool)
-qBoundary = gradY * boundaryMap.astype(bool)
-
+# gradX = gradX * boundaryMap.astype(bool)
+# gradY = gradY * boundaryMap.astype(bool)
+pBoundary = np.array(gradX * boundaryMap.astype(bool),copy=True)
+qBoundary = np.array(gradY * boundaryMap.astype(bool),copy=True)
+pBoundary = pBoundary - pBoundary * (1 - boundaryMap)
+qBoundary = qBoundary - qBoundary * (1 - boundaryMap)
 # print(pBoundary)
 
 #######################################################
 # Iterative Shape from shading
 #######################################################
-limit = 10
-p_next,q_next = pBoundary,qBoundary
-p_estimated,q_estimated = p_next,q_next	
+limit = 100
+p_next,q_next = np.array(pBoundary,copy=True),np.array(qBoundary,copy=True)
+p_estimated,q_estimated = np.array(pBoundary,copy=True),np.array(qBoundary,copy=True)	
 
 for iteration in range(0,limit):
 	print('Starting Iteration :', iteration+1)
@@ -95,8 +114,8 @@ for iteration in range(0,limit):
 				radianc = (p_estimated[i][j] * source[0] + q_estimated[i][j] * source[1] + 1) / ( ((source[0]**2+source[1]**2 + 1)**0.5) *((p_estimated[i][j]**2 + q_estimated[i][j]**2 + 1)**0.5))
 				RadianceX = (p_estimated[i][j]**2 *source[0] + source[0] - q_estimated[i][j]*q_estimated[i][j]*source[1] - p_estimated[i][j])/(((source[0]**2 + source[1]**2 + 1)**0.5)*(p_estimated[i][j]**2 + (q_estimated[i][j]**2 + 1)**0.5)**3)
 				RadianceY = (q_estimated[i][j]**2 *source[1] + source[1] - p_estimated[i][j]*p_estimated[i][j]*source[0] - q_estimated[i][j])/(((source[0]**2 + source[1]**2 + 1)**0.5)*(p_estimated[i][j]**2 + (q_estimated[i][j]**2 + 1)**0.5)**3)
-				p_next[i][j] = 0.25*(p_estimated[i-1][j] + p_estimated[i+1][j] + p_estimated[i][j-1] + p_estimated[i][j+1]) + 1/Lambda *(radiance[i][j] - radianc)*RadianceX;
-				q_next[i][j] = 0.25*(q_estimated[i-1][j] + q_estimated[i+1][j] + q_estimated[i][j-1] + q_estimated[i][j+1]) + 1/Lambda *(radiance[i][j] - radianc)*RadianceY;
+				p_next[i][j] = 0.25*(p_estimated[i-1][j] + p_estimated[i+1][j] + p_estimated[i][j-1] + p_estimated[i][j+1]) - 1/Lambda *(radiance[i][j] - radianc)*RadianceX;
+				q_next[i][j] = 0.25*(q_estimated[i-1][j] + q_estimated[i+1][j] + q_estimated[i][j-1] + q_estimated[i][j+1]) - 1/Lambda *(radiance[i][j] - radianc)*RadianceY;
 	p_estimated = (p_next*regionOfInterestRadiance*(1-boundaryMap.astype(bool))) + pBoundary*boundaryMap*regionOfInterestRadiance
 	q_estimated = (q_next*regionOfInterestRadiance*(1-boundaryMap.astype(bool))) + qBoundary*boundaryMap*regionOfInterestRadiance
 
@@ -108,27 +127,32 @@ for iteration in range(0,limit):
 limit = 100
 Z_p   = np.zeros(p_estimated.shape)
 Z     = np.zeros(p_estimated.shape)
-p_x,q_y = p_estimated,q_estimated
-p_x[:][1:-1] = ( p_estimated[:][2:] - p_estimated[:][:-2])*0.1;
-q_y[1:-1][:] = ( q_estimated[:-2][:] - q_estimated[2:][:])*0.1;
+p_x,q_y = np.array(p_estimated,copy=True),np.array(q_estimated,copy=True)
+p_x[:][1:-1] = ( p_estimated[:][2:] - p_estimated[:][:-2]);
+q_y[1:-1][:] = ( q_estimated[:-2][:] - q_estimated[2:][:]);
 for iteration in range(0,limit):
 	for i in range(1,p_estimated.shape[0]-1):
 		for j in range(1,p_estimated.shape[1]-1):
 			if regionOfInterestRadiance[i][j] == 1 :
-				Z[i][j] = 0.5*( Z_p[i-1][j] + Z_p[i+1][j] + Z_p[i][j-1] + Z_p[i][j+1]) - abs(p_x[i][j]) - abs(q_y[i][j])
+				Z[i][j] = 0.25*( Z_p[i-1][j] + Z_p[i+1][j] + Z_p[i][j-1] + Z_p[i][j+1]) + abs(p_x[i][j]) + abs(q_y[i][j])
 	Z_p = regionOfInterestRadiance*Z;
 
 Z_estimated = Z * regionOfInterestRadiance
-Z_estimated = -Z_estimated
+#Z_estimated = Z_estimated
+
+print(np.amax(Z_estimated))
 
 # print(q_y[sphereImageSize/2])
-print(Z_p[sphereImageSize/2])
+print(p_x[sphereImageSize/2])
 # print(p_x[sphereImageSize/2])
 #######################################################
 # Visualization of the Depth
 #######################################################
 fig = plt.figure()
 ax = fig.gca(projection='3d')
-surf = ax.plot_surface(rows, cols, Z_estimated, rstride=1, cstride=1, cmap=cm.coolwarm,linewidth=0, antialiased=False)
-fig.colorbar(surf, shrink=0.5, aspect=5)
+ax.set_xlim3d(0,sphereImageSize)
+ax.set_ylim3d(0,sphereImageSize)
+ax.set_zlim3d(0,sphereImageSize)
+surf = ax.plot_surface(rows, cols, Z_estimated/4, rstride=1, cstride=1, cmap=cm.coolwarm,linewidth=0, antialiased=False)
+fig.colorbar(surf, shrink=1, aspect=5)
 plt.show()
