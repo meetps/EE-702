37c6b91eaa1301e2a99358f5cb6b9ff65b9cface
yashbhalgat <yashbhalgat95@gmail.com>
2016-02-11 03:04:48 +0530

    working for images

--- shape-from-shading/shape_from_shading_image.py
+++ shape-from-shading/shape_from_shading_image.py
@@ -73,10 +73,14 @@ for i in range(0,sphereImageSize):
 # Detecting Boundary using Morphological Operations from OpenCV
 #######################################################
 
-# rad = cv2.imread('im2.JPG')
-# rad = cv2.cvtColor(rad, cv2.COLOR_RGB2GRAY)
-# radiance = np.asarray(rad)
-# plt.imshow(radiance)
+rad = cv2.imread('im2.JPG')
+rad = cv2.cvtColor(rad, cv2.COLOR_RGB2GRAY)
+rad = cv2.resize(rad,None,fx=0.05,fy=0.05,interpolation=cv2.INTER_CUBIC)
+radiance = np.asarray(rad)
+plt.imshow(radiance)
+
+regionOfInterest = radiance > 0
+regionOfInterestRadiance = radiance > 0
 
 boundaryMap              = np.zeros((sphereImageSize,sphereImageSize))
 regionOfInterestRadiance = radiance > 0
@@ -84,8 +88,8 @@ kernel                   = np.ones((3,3),np.uint8)
 boundaryMap              = regionOfInterestRadiance - cv2.erode(regionOfInterestRadiance.astype(np.uint8),kernel,5)
 boundaryMap              = cv2.erode(cv2.dilate(boundaryMap.astype(np.uint8),kernel,3),kernel,3) 
 intersectionROI          = regionOfInterest * regionOfInterestRadiance
-p,q = p * intersectionROI , q * intersectionROI
-
+# p,q = p * intersectionROI , q * intersectionROI
+# plt.imshow(boundaryMap)
 # print(sum(sum(boundaryMap)))
 
 
@@ -108,7 +112,7 @@ qBoundary = qBoundary - qBoundary * (1 - boundaryMap)
 #######################################################
 # Iterative Shape from shading
 #######################################################
-limit = 1000
+limit = 5000
 p_next,q_next = np.array(pBoundary,copy=True),np.array(qBoundary,copy=True)
 p_estimated,q_estimated = np.array(pBoundary,copy=True),np.array(qBoundary,copy=True)	
 
@@ -131,7 +135,7 @@ for iteration in range(0,limit):
 #######################################################
 # Depth Retrieval 
 #######################################################
-limit = 1000
+limit = 5000
 Z_p   = np.zeros(p_estimated.shape)
 Z     = np.zeros(p_estimated.shape)
 p_x,q_y = np.array(p_estimated,copy=True),np.array(q_estimated,copy=True)
@@ -145,6 +149,7 @@ for iteration in range(0,limit):
 	Z_p = regionOfInterestRadiance*Z;
 
 Z_estimated = Z * regionOfInterestRadiance
+Z_estimated = 100*Z_estimated/np.amax(Z_estimated)
 #Z_estimated = Z_estimated
 
 print(np.amax(radiance))
@@ -156,11 +161,14 @@ print(np.amin(radiance))
 #######################################################
 # Visualization of the Depth
 #######################################################
+
+plt.imshow(Z_estimated)
 fig = plt.figure()
 ax = fig.gca(projection='3d')
-ax.set_xlim3d(0,sphereImageSize)
-ax.set_ylim3d(0,sphereImageSize)
-ax.set_zlim3d(0,sphereImageSize)
+# ax.set_xlim3d(0,sphereImageSize)
+# ax.set_ylim3d(0,sphereImageSize)
+# ax.set_zlim3d(0,sphereImageSize)
+[rows,cols] = np.meshgrid(range(0,rad.shape[1]),range(0,rad.shape[0]))
 surf = ax.plot_surface(rows, cols, Z_estimated, rstride=1, cstride=1, cmap=cm.coolwarm,linewidth=0, antialiased=False)
 fig.colorbar(surf, shrink=1, aspect=5)
 plt.show()
