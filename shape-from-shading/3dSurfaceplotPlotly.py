f30097fd9e18b20629911a360101d8e214369f7b
meetshah1995 <meetshah1995@iitb.ac.in>
2016-02-15 23:29:41 +0530

    Add plotly script

--- shape-from-shading/3dSurfaceplotPlotly.py
+++ shape-from-shading/3dSurfaceplotPlotly.py
@@ -1,9 +1,12 @@
 import plotly.plotly as py
 import plotly.graph_objs as go
 import numpy as np
+import plotly.tools as tls
+tls.set_credentials_file(username='meetshah1995efe3', api_key='mxg1wama31')
 
+a = np.load('results/r_50nr_0ns_0lambda_100.npy')
 
-data = [go.Surface()]
+data = [go.Surface(z=a)]
 
 layout = go.Layout(
     title='Shape from Shading',
