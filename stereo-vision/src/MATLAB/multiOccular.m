f4a63670c51ae829680658cefc2c8f182ad44b10
meetshah1995 <meetshah1995@iitb.ac.in>
2016-03-14 15:36:30 +0530

    Add multi occular stereo vision

---
+++ stereo-vision/src/MATLAB/multiOccular.m
@@ -0,0 +1,13 @@
+v0 = imread('../../data/Aloe/view2.png');
+v1 = imread('../../data/Aloe/view3.png');
+v2 = imread('../../data/Aloe/view4.png');
+v3 = imread('../../data/Aloe/view5.png');
+
+d1 = stereo_SAD(v2, v3, 16);
+d2 = stereo_SAD(v1, v3, 2*16);
+d3 = stereo_SAD(v0, v3, 3*16);
+
+%% Results good till 372
+depth = (d1/1 + d2/2 + d3/3 )/3;
+
+imshow(mat2gray(depth),[]);
